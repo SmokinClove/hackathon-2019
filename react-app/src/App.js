2ce8a99754bb6b7dfce9c95d77a74d1fbc3e027c
Chong Jia Wei <jiawei.chong@jiaweichong-mac.local>
2019-06-21 16:04:56 +0800

    Add Rect component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Components from './Components';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const canvas = new Components();
+  canvas.addRect(100, 100, 500, 150);
+  return <></>
 }
 
 export default App;
