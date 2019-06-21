aa71e22bd9be9ec04b07b194585f3a37b393c6a7
heyjiawei <jaewei.j.w@gmail.com>
2019-06-21 17:06:39 +0800

    Add Circle, Triangle and Diamond

--- react-app/src/Components/index.js
+++ react-app/src/Components/index.js
@@ -1,6 +1,11 @@
 import { fabric } from "fabric";
 
 let canvas = null;
+const inlineProperties = {
+	fill: 'transparent',
+	stroke: 'black',
+	strokeWidth: 5
+}
 
 export default class Component {
   constructor() {
@@ -27,11 +32,52 @@ export default class Component {
       top: y1,
       width,
 			height,
-			fill: 'transparent',
-			stroke: 'black',
-    	strokeWidth: 5
+			...inlineProperties
 		});
 		canvas.add(rect);
+		return this;
+	}
+
+	addCircle(x1, y1, x2, y2) {
+		const radius = Math.min(x2 - x1, y1 - y2) / 2;
+		const circle = new fabric.Circle({
+			left: x1, 
+			top: y1,
+			radius,
+			...inlineProperties
+		});
+		canvas.add(circle);
+		return this;
+	}
+
+	addTriangle(x1, y1, x2, y2) {
+		const triangle = new fabric.Triangle({
+			width: x2 - x1, 
+			height: y1 - y2, 
+			left: x1,
+			top: y1,
+			...inlineProperties
+		})
+		.set('flipY', true);
+		canvas.add(triangle);
+		return this;
+	}
+
+	addDiamond(x1, y1, x2, y2) {
+		const midX = x1 + (x2 - x1) / 2,
+		midY = y2 + (y1 - y2) / 2;
+
+		const poly = new fabric.Polygon([
+				{ x: midX, y: y1 },
+				{ x: x2, y: midY },
+				{ x: midX, y: y2 },
+				{ x: x1, y: midY },
+			], {
+			left: x1,
+			top: y1,
+			...inlineProperties
+		});
+		canvas.add(poly);
 		return this;
 	}
 	
