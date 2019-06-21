f9766cfa4fda9dce867b905007f786b02e61ad17
Nguyen Tuong Van <tuongvan.nguyen@shopee.com>
2019-06-21 15:40:46 +0800

    Add code for forming object

--- react-app/src/Draw.js
+++ react-app/src/Draw.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+/*
+Point : {x : number, y: number}
+ */
 export default class Draw extends React.Component {
   constructor(props) {
     super(props);
     this.canvas = React.createRef();
+    this.obj = []; // an Array of Array of Points, which is the object to draw
+    this.currentLine = []; // Stores an array of Points, which is the current line being drawn
+    setInterval(this.formObject, 2000);
+  }
+
+  formObject = () => {
+    // TODO: send to api and refresh the this.obj
+    console.log('obj ', this.obj);
+    const timestamp = new Date().getTime();
+    // getImage(timestamp, this.obj)
+    this.obj = [];
   }
 
   componentDidMount() {
@@ -11,65 +25,64 @@ export default class Draw extends React.Component {
     var clickY = [];
     var clickDrag = [];
     var paint, context;
+    var self = this;
 
     const addClick = (x, y, dragging) => {
-        clickX.push(x);
-        clickY.push(y);
-        clickDrag.push(dragging);
+      clickX.push(x);
+      clickY.push(y);
+      this.currentLine.push({x, y});
+      clickDrag.push(dragging);
     }
 
     const redraw = () => {
-        context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
 
-        context.strokeStyle = "#111";
-        context.lineJoin = "round";
-        context.lineWidth = 5;
+      context.strokeStyle = "#111";
+      context.lineJoin = "round";
+      context.lineWidth = 5;
 
-        for (var i = 0; i < clickX.length; i++) {
-            context.beginPath();
-            if (clickDrag[i] && i) {
-                context.moveTo(clickX[i - 1], clickY[i - 1]);
-            } else {
-                context.moveTo(clickX[i] - 1, clickY[i]);
-            }
-            context.lineTo(clickX[i], clickY[i]);
-            context.closePath();
-            context.stroke();
+      for (var i = 0; i < clickX.length; i++) {
+        context.beginPath();
+        if (clickDrag[i] && i) {
+          context.moveTo(clickX[i - 1], clickY[i - 1]);
+        } else {
+          context.moveTo(clickX[i] - 1, clickY[i]);
         }
+        context.lineTo(clickX[i], clickY[i]);
+        context.closePath();
+        context.stroke();
+      }
     }
 
-    const app = () => {
-      if(!this.canvas) return;
-        const canvas = this.canvas.current;
-        context = canvas.getContext("2d");
-        console.log('createdContext');
-        canvas.addEventListener("mousedown", function (e) {
-            paint = true;
-            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-            redraw();
-        })
 
-        canvas.addEventListener("mousemove", function (e) {
-            if (paint) {
-                addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-                redraw();
-            }
-        });
+    if(!this.canvas) return;
+    const canvas = this.canvas.current;
+    context = canvas.getContext("2d");
+    canvas.addEventListener("mousedown", function (e) {
+      paint = true;
+      addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+      redraw();
+    })
 
-        canvas.addEventListener("mouseup", function(e){
-            paint = false;
-        })
+    canvas.addEventListener("mousemove", function (e) {
+      if (paint) {
+        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+        redraw();
+      }
+    });
 
-        canvas.addEventListener("mouseleave", function (e) {
-            paint = false;
-        });
+    canvas.addEventListener("mouseup", function(e){
+      paint = false;
+      if(!!self.currentLine && self.currentLine.length > 0)self.obj.push(self.currentLine);
+      self.currentLine = [];
+    });
 
-    }
-
-    app();
+    canvas.addEventListener("mouseleave", function (e) {
+      paint = false;
+    });
   }
+
   render() {
     return  <div><canvas id="canvasInAPerfectWorld" width="600" height="600" style={{border: "black 1px solid"}} ref={this.canvas}></canvas></div>
   }
-
 }
