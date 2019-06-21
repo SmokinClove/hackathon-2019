098815177e3ed6ed7575ebe0c31f1c1684801218
Zhi Yang Ten <tenzy@seagroup.com>
2019-06-21 14:17:26 +0800

    added quickdraw wrapper

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,17 @@
 
 import express from 'express';
+import mobileNet from './mobilenet'
 var router = express.Router();
+
+// const SAVE = '/save';
+const INFER = '/infer';
 
 router.get('/', function (req, res) {
     res.json({ message: 'API entry point is /api/<object>' })
 });
+
+router.route(INFER).post(mobileNet.log);
+router.route(INFER).get(mobileNet.log);
 //for every new schema, write routes for them
 //import functions from controller file
 // var exampleController = require('../controllers/example.controller.js');
